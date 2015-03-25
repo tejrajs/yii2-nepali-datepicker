472ffc68d589bcd8789786e3fe9043c1849b3be6
tejrajs <helloteju96@gmail.com>
2015-03-25 12:29:05 +0545

    assets and file added

---
+++ DatePickerAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @link http://2amigos.us
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace tejrajs\datepicker;
+use yii\web\AssetBundle;
+/**
+ * DatePickerAsset
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @package dosamigos\datepicker
+ */
+class DatePickerAsset extends AssetBundle
+{
+   public $sourcePath = '@vendor/tejrajs/yii2-fontawesome/assets';
+   public $css = [	'nepali.datepicker.css'	];
+   public $css = [	'nepali.datepicker.js'	];
+}
